23cba124aac2d5ec594f21f966abd1247c30308a
Wakaru Himura <wakaru44@gmail.com>
2014-06-18 06:22:40 +0200

    trials with travis 2

--- run_tests.sh
+++ run_tests.sh
@@ -6,12 +6,13 @@ nosetests tests/test_parsers.py
 
 echo "testing environment"
 
+cd google_appengine
 ls -lart
 
 if [ $(grep "^version:" app.yaml | cut -d " " -f 2) -eq 2 ]
 then
 	echo "Dev version"
-	appcfg.py update --oauth2 --noauth_local_webserver
+	appcfg.py update --oauth2 --noauth_local_webserver ..
 else
 	echo "Non dev version"
 fi
