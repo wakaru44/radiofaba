ab1b7d5eb80d4f193f7e44cd97690140d5c01118
Wakaru Himura <wakaru44@gmail.com>
2014-06-18 07:32:03 +0200

    trials with travis 7

--- run_tests.sh
+++ run_tests.sh
@@ -10,15 +10,15 @@ echo "testing environment"
 if [ $(grep "^version:" app.yaml | cut -d " " -f 2) -eq 2 ]
 then
 	echo "Dev version"
-	cd google_appengine
-	echo " google_appengine"
-	ls -lart
-	echo "hostname"
-	hostname
-	ifconfig
+	#cd google_appengine
+	#echo " google_appengine"
+	#ls -lart
+	#echo "hostname"
+	#hostname
+	#ifconfig
 	# oauth code:
-	token="4/9JwInYmiQAdaLh9xqDSHDavVyYfL.kqeQpM1lKmofOl05ti8ZT3a3FIpajQI"
-	python appcfg.py update --oauth2 --noauth_local_webserver --oauth2_access_token=$token ..
+	#token="4/9JwInYmiQAdaLh9xqDSHDavVyYfL.kqeQpM1lKmofOl05ti8ZT3a3FIpajQI"
+	#python appcfg.py update --oauth2 --noauth_local_webserver --oauth2_access_token=$token ..
 else
 	echo "Non dev version"
 fi
