615aa40f843231ffe6679d1460266a3411d7d25d
Wakaru Himura <wakaru44@gmail.com>
2014-06-18 02:46:59 +0200

    experimenting with self-deploy

--- run_tests.sh
+++ run_tests.sh
@@ -3,3 +3,11 @@
 echo "Testing bash execution on travis"
 
 nosetests tests/test_parsers.py
+
+if [ $(grep "^version:" app.yaml | cut -d " " -f 2) -eq 2 ]
+then
+	echo "Dev version"
+	appcfg.py update --oauth2 --noauth_local_webserver
+else
+	echo "Non dev version"
+fi
